2608dee482a8468af1cb62245bc1792142c292d3
leemera <jseejy57@gmail.com>
2025-04-23 23:15:12 +0900

    ok

--- 3일차 과제/app.py
+++ 3일차 과제/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_smorest import Api
-from flask_sqlalchemy import SQLAlchemy
 from db import db
 from models import User, Board
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)x
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/oz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
